45a41605f52b1494bec3b490089713dc21a5cc55
Hiroshi Miyazawa <gdgdkuro@gmail.com>
2021-05-03 14:36:41 +0900

    ユニティちゃんのアセットを追加

---
+++ unity_project/Assets/UnityChan/SplashScreen/Scripts/SplashScreen.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace UnityChan
+{
+	[ExecuteInEditMode]
+	public class SplashScreen : MonoBehaviour
+	{
+		void NextLevel ()
+		{
+            // 旧形式の警告が出ていたため、書き換え(動作確認はしていない)
+            UnityEngine.SceneManagement.SceneManager.LoadScene(UnityEngine.SceneManagement.SceneManager.GetActiveScene().name + 1);
+			//Application.LoadLevel(Application.loadedLevel + 1);
+		}
+	}
+}
