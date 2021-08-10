8569085e890475622dc0dacb350feec1bbf7d006
gdgdmh <gdgdkuro@gmail.com>
2021-08-10 13:23:36 +0900

    シングルトンクラスを仮作成

---
+++ unity_project/Assets/Mhl/System/Singleton/MhlSingleton.cs
@@ -0,0 +1,35 @@
+using System.Collections.Generic;
+
+namespace Mhl.System.Singleton
+{
+    /// <summary>
+    /// シングルトンベースクラス
+    /// デフォルトコンストラクタで生成可能なクラスに対してシングルトンで扱う
+    /// </summary>
+    /// <typeparam name="Type">シングルトンにするクラス</typeparam>
+    public class MhlSingleton<Type> where Type : class, new()
+    {
+        static private bool initializeFlag = false;
+        static private Type singletonObject = null;
+
+        public static Type GetInstance()
+        {
+            if (!initializeFlag)
+            {
+                Create();
+            }
+            return singletonObject;
+        }
+
+        private static void Create()
+        {
+            singletonObject = new Type();
+            // クリーンアップ関数に登録
+        }
+
+        private static void Cleanup()
+        {
+            singletonObject = null;
+        }
+    }
+}
